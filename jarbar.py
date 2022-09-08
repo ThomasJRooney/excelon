f637b6f53b90f141ef497a9190eb27d789773a55
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-09-08 16:40:31 -0500

    Positive Nums Test Passing

--- jarbar.py
+++ jarbar.py
@@ -7,7 +7,18 @@ class JarBar():
             return ""
         if self.num == 0:
             return ""
-        
+
+        ret = ""
+        for n in range(1, self.num + 1):
+            if n % 3 == 0 and n % 5 == 0:
+                ret += "JarBar\n"
+            elif n % 3 == 0:
+                ret += "Jar\n"
+            elif n % 5 == 0:
+                ret += "Bar\n"
+            else:
+                ret += str(n) + "\n"
+        return ret
 
 import unittest
 
@@ -29,7 +40,7 @@ class Test(unittest.TestCase):
         self.assertEqual(test1.to_string(), "1\n2\nJar\n4\nBar\n")
         
         test2 = JarBar(15)
-        self.assertEqual(test2.to_string(), "1\n2\nJar\n4\nBar\nJar\n7\n8\nJar\nBar\n11\nJar\n13\n14\nJarBar")
+        self.assertEqual(test2.to_string(), "1\n2\nJar\n4\nBar\nJar\n7\n8\nJar\nBar\n11\nJar\n13\n14\nJarBar\n")
 
 if __name__ == '__main__':
     unittest.main()
